70d4a2c2a7cf636dbb81a00e23d38fdf8c89c5a1
NguyenDucHuy202 <Nguyenduchuy702002@gmail.com>
2024-10-22 10:43:00 +0700

    Component ? làm sao sử dụng ? Loại Component ?

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './Example/MyComponent';
+
+/**
+ *  có 2 loại componet: class component / funtion component
+ * Funtion ( funtion / arrow funtion)
+ * funtion: funtion _FuntionName_():...
+ * arrow funtion : const/let _VaLue_ = () =>
+ * 
+ */
 
 function App() {
+  // JSX -> return 1 cục html -> component
+  // Biến = thẻ html được luôn 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           heeloo React
+          
         </p>
         <a
           className="App-link"
@@ -17,6 +29,9 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent/>
+
+       
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,15 @@
+import React from "react";  
+
+class MyComponent extends React.Component
+{
+    render()
+    {
+        return(
+
+            <div>Hello Nguyễn Đức Huy</div>
+        )
+    }
+}
+
+
+export default MyComponent;
